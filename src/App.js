56b5a3c537ee15e074c57e41b34725d99fab79cb
Takeda Chiharu <takeda123pc@gmail.com>
2020-11-27 12:35:42 +0900

    show search results

--- src/App.js
+++ src/App.js
@@ -1,55 +1,71 @@
 import client from './client'
 import { ApolloProvider } from 'react-apollo'
-import {SEARCH_REPOSITORIES} from "./graphql";
+import { SEARCH_REPOSITORIES } from "./graphql";
 import { Query } from "react-apollo";
-import {useState} from "react";
+import { useState } from "react";
 
 
 function App() {
 
-  const [DEFAULT_STATE,handleChange] = useState({
+  const [DEFAULT_STATE, handleChange] = useState({
     first: 5,
     after: null,
     last: null,
     before: null,
     query: "フロントエンドエンジニア"
   });
-  
+
   const setQueryWrap = (e) => {
-      const queryValue = e.target.value
-      handleChange({
-        ...DEFAULT_STATE,
-        query:queryValue
-      });
+    const queryValue = e.target.value
+    handleChange({
+      ...DEFAULT_STATE,
+      query: queryValue
+    });
   };
 
   const handleSubmit = (e) => {
     e.preventDefault()
   }
-  
-  const {query,first,last,before,after} = DEFAULT_STATE;
-  
+
+  const { query, first, last, before, after } = DEFAULT_STATE;
+
   console.log(query)
   return (
     <ApolloProvider client={client}>
       <form onSubmit={handleSubmit}>
-        <input value={query} onChange={setQueryWrap}/>
+        <input value={query} onChange={setQueryWrap} />
       </form>
-      <Query 
+      <Query
         query={SEARCH_REPOSITORIES}
-        variables= {{query,first,last,before,after}}
+        variables={{ query, first, last, before, after }}
       >
         {
-          ({loading,error,data})=>{
-            if(loading) return 'Loading...'
-            if(error) return `Error... ${error.message}`
-            console.log({data})
+          ({ loading, error, data }) => {
+            if (loading) return 'Loading...'
+            if (error) return `Error... ${error.message}`
+            console.log({ data })
 
             const search = data.search
             const repositoryCount = search.repositoryCount
-            const repositoryUnit = repositoryCount === 1 ? 'Repository':"Repositories"
+            const repositoryUnit = repositoryCount === 1 ? 'Repository' : "Repositories"
             const title = `GitHub repositories Search Results - ${repositoryCount} ${repositoryUnit}`
-            return <h2>{title}</h2>
+            return (
+              <>
+                <h2>{title}</h2>
+                <ul>
+                  {
+                    search.edges.map((edge) => {
+                      const node = edge.node
+                      return (
+                        <li key={node.id}>
+                          <a href={node.url} target="_blank" rel="noreferrer">{node.name}</a>
+                        </li>
+                      )
+                    })
+                  }
+                </ul>
+              </>
+            )
           }
         }
       </Query>
